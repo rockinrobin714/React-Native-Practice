fa279ede16f173151a8fc6629a12e5e55be5c4d8
rockinrobin714 <robindykema@gmail.com>
2016-12-16 14:02:40 -0600

    Added input

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import firebase from 'firebase';
+
+import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyDxf0FnDuwKA2loucxUc0yoZ11oiaE2ikU",
+      authDomain: "auth-b1d01.firebaseapp.com",
+      databaseURL: "https://auth-b1d01.firebaseio.com",
+      storageBucket: "auth-b1d01.appspot.com",
+      messagingSenderId: "650973154753"
+    })
+  }
   render() {
     return (
       <View>
-        <Text>Hi</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
       )
   }
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,44 +1,40 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
-  const { inputStyle, labelStyle, containerStyle } = styles;
-
+const Input = ({label, value, placeholder, onChangeText}) => {
+  const {inputStyle, labelStyle, containerStyle} = styles;
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
-      <TextInput
-        secureTextEntry={secureTextEntry}
-        placeholder={placeholder}
+      <TextInput style={inputStyle}
         autoCorrect={false}
-        style={inputStyle}
+        placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
       />
     </View>
-  );
+    )
 };
 
 const styles = {
-  inputStyle: {
-    color: '#000',
-    paddingRight: 5,
-    paddingLeft: 5,
-    fontSize: 18,
-    lineHeight: 23,
-    flex: 2
-  },
   labelStyle: {
-    fontSize: 18,
-    paddingLeft: 20,
+    fontSize:18,
+    paddingLeft:20,
     flex: 1
   },
+  inputStyle: {
+    color:'#000',
+    paddingRight:5,
+    paddingLeft:5,
+    fontSize:18,
+    lineHeight: 23,
+    flex:2
+  },
   containerStyle: {
-    height: 40,
-    flex: 1,
+    height:40,
+    flex:1,
     flexDirection: 'row',
     alignItems: 'center'
   }
 };
-
 export { Input };
